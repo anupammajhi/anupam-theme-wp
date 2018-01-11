eadbb1b942f6dea76474b92cd177b0f4b4331826
Anupam Majhi <majhi.anupam@gmail.com>
2018-01-11 05:59:09 +0530

    Add code tag styling

--- header-blog.php
+++ header-blog.php
@@ -8,6 +8,7 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,900" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:400,900" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/img/icons/icon-works_v1/stylesheet.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
