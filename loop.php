503fbee63f9fddd8e791c830fdfa44583fd3194a
Anupam Majhi <majhi.anupam@gmail.com>
2018-01-12 02:32:13 +0530

    Custom layout for tags and categories

--- loop.php
+++ loop.php
@@ -4,26 +4,29 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<!-- post thumbnail -->
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+		<div class="post-thumb">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+				<div class="post-thumb-img">
+				<?php if ( has_post_thumbnail()) : // Check if thumbnail exists 
+							the_post_thumbnail('large');
+					  else: //if no thumbnail?>
+						<img src="<?php echo get_template_directory_uri(); ?>/img/default-thumb.jpeg">
+				<?php endif; ?>
 			</a>
-		<?php endif; ?>
+		</div>
 		<!-- /post thumbnail -->
 
 		<!-- post title -->
-		<h2>
+		<h2 class="blog-title">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 		</h2>
 		<!-- /post title -->
 
 		<!-- post details -->
 		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
 		<!-- /post details -->
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+		<div class="post-excerpt"><?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?></div>
 
 		<?php edit_post_link(); ?>
 
--- tag.php
+++ tag.php
@@ -1,15 +1,98 @@
-<?php get_header(); ?>
+<?php get_header('blogpost'); ?>
+
+<style type="text/css">
+	main{
+		color: #666;
+		font-size: 1.5em;
+	}
+	main .iconworks{
+	text-align: center;
+	font-size: 3em;
+	font-style: normal;
+	display: block;
+	margin:0;
+	}
+	main h1{
+	text-align: center;
+	font-size: 2em;
+	font-style: normal;
+	margin:0;
+	font-weight:900;
+	line-height:0.5;
+	}
+	.head-box{
+		margin-top:30px;
+	}
+	article{
+		display:inline-block;
+		width : 30.5%;
+		padding: 5px;
+		margin: 1%;
+		vertical-align:top;
+	}
+	.category-box{
+		margin-top: 2em; 
+	}
+	.post-excerpt p{
+		margin-top : 7px;
+	}
+	.post-thumb{
+	}
+	.post-thumb-img{
+		height:12em;
+		overflow:hidden;
+	}
+	.post-thumb-img img{
+		width:100%;
+		min-height:100%;
+	}
+	.blog-title{
+		font-size:1em;
+		margin-bottom: 5px;
+	}
+	.date{
+		font-size:0.8em;
+		margin: -12px 0;;
+	}
+	.pagination{
+		text-align:center;
+	}
+	.page-numbers{
+		padding:5px 12px;
+		color:#333;
+		background: #ccc;
+	}
+	.page-numbers.current{
+		color:#ccc;
+		background: #333;
+	}
+	.next.page-numbers,
+	.previous.page-numbers{
+		padding:5px;
+		margin:5px;
+	}
+	.pagination-box{
+		margin:50px;
+	}
+	
+</style>
+
 
 	<main role="main">
-		<!-- section -->
-		<section>
+	<!-- section -->
+	<section>
 
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+		<div class="head-box">
+		<i class="iconworks" data-icon="&#99;"></i>
+		<h1><?php _e( 'Posts with Tag: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+		</div>
 
+		<div class="category-box">
 			<?php get_template_part('loop'); ?>
-
+		</div>
+		<div class="pagination-box">
 			<?php get_template_part('pagination'); ?>
-
+		</div>
 		</section>
 		<!-- /section -->
 	</main>
@@ -17,3 +100,4 @@
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
